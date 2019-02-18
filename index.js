7500fc91f0da3ab16ef033bdf3c949137eeb78e4
Imon Ovbude <imonovbude@gmail.com>
2019-02-18 12:24:44 -0800

    setup index.js to run server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+// MIDDLEWARE
+server.use(express.json());
+
+
+
+// PORT LISTENER
+server.listen(5000, () => {
+    console.log('server Running on http://localhost:5000')
+});
